4abd409acd7423b7363564fd649dbc155f18346b
RanGooooo <13835218803@163.com>
2019-11-02 06:32:10 +0800

    Initial commit

---
+++ src/test/java/com/rain/bjlj/BjljApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rain.bjlj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BjljApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
